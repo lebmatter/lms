4c9feef3eecf6694738112e95765ea529732adb0
Labeeb Mattra <mmlabeeb@gmail.com>
2023-07-07 12:58:12 +0530

    exams portal page for listing all exams

---
+++ lms/www/exams/index.py
@@ -0,0 +1,61 @@
+import frappe
+from frappe import _
+from lms.lms.utils import (
+	# Exams and Course ceators share same role
+	can_create_courses,
+	check_profile_restriction,
+	get_restriction_details,
+	has_course_moderator_role,
+	get_exams_under_review,
+)
+from lms.overrides.user import get_registered_exams, get_authored_exams
+
+
+def get_context(context):
+	context.no_cache = 1
+	context.live_exams, context.upcoming_exams = get_exams()
+	context.enrolled_exams = (
+		get_registered_exams()["in_progress"] + get_registered_exams()["completed"]
+	)
+	context.created_courses = get_authored_exams(None, False)
+	context.review_courses = get_exams_under_review()
+	context.restriction = check_profile_restriction()
+	context.show_creators_section = can_create_courses()
+	context.show_review_section = (
+		has_course_moderator_role() and frappe.session.user != "Guest"
+	)
+
+	if context.restriction:
+		context.restriction_details = get_restriction_details()
+
+	context.metatags = {
+		"title": _("Exam List"),
+		"image": frappe.db.get_single_value("Website Settings", "banner_image"),
+		"description": "This page lists all the exams published on our website",
+		"keywords": "All Exams, Exams, Learn",
+	}
+
+
+def get_exams():
+	exams = frappe.get_all(
+		"LMS Exam",
+		filters={"published": True},
+		fields=[
+			"name",
+			"upcoming",
+			"title",
+			"image",
+			"enable_certification",
+			"paid_certificate",
+			"price_certificate",
+			"currency",
+		],
+	)
+
+	live_exams, upcoming_courses = [], []
+	for exam in exams:
+		if exam.upcoming:
+			upcoming_courses.append(exam)
+		else:
+			live_exams.append(exam)
+	return live_exams, upcoming_courses
