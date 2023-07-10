31154841cf934f893285107896d4854256c4079e
Labeeb Mattra <mmlabeeb@gmail.com>
2023-07-10 18:58:09 +0530

    Add exam.html page

---
+++ lms/www/exams/exam.py
@@ -0,0 +1,95 @@
+import frappe
+from frappe import _
+
+from lms.lms.utils import (
+	get_evaluation_details,
+	get_exam_registration,
+	has_course_moderator_role,
+	is_certified,
+	is_instructor,
+	redirect_to_exams_list,
+	get_exam_average_rating,
+)
+
+
+def get_context(context):
+	context.no_cache = 1
+
+	try:
+		exam_name = frappe.form_dict["exam"]
+		print("#"*100, exam_name)
+	except KeyError:
+		redirect_to_exams_list()
+
+	set_exam_context(context, exam_name)
+	context.avg_rating = get_exam_average_rating(context.exam.name)
+
+
+def set_exam_context(context, exam_name):
+	exam = frappe.db.get_value(
+		"LMS Exam",
+		exam_name,
+		[
+			"name",
+			"title",
+			"image",
+			"short_introduction",
+			"description",
+			"published",
+			"upcoming",
+			"disable_self_learning",
+			"status",
+			"video_link",
+			"enable_certification",
+			"grant_certificate_after",
+			"paid_certificate",
+			"price_certificate",
+			"currency",
+			"max_attempts",
+			"duration",
+		],
+		as_dict=True,
+	)
+
+	if frappe.form_dict.get("edit"):
+		if not is_instructor(exam.name) and not has_course_moderator_role():
+			raise frappe.PermissionError(_("You do not have permission to access this page."))
+		exam.edit_mode = True
+
+	if exam is None:
+		raise frappe.PermissionError(_("This is not a valid course URL."))
+
+	related_courses = frappe.get_all(
+		"Related Courses", {"parent": exam.name}, ["course"]
+	)
+	for csr in related_courses:
+		csr.update(
+			frappe.db.get_value(
+				"LMS Course",
+				csr.course,
+				["name", "upcoming", "title", "image", "enable_certification"],
+				as_dict=True,
+			)
+		)
+	exam.related_courses = related_courses
+
+	context.exam = exam
+	# registartion = get_exam_registration(exam.name, frappe.session.user)
+	# context.registartion = registartion
+	context.certificate = is_certified(exam.name)
+	if context.exam.upcoming:
+		context.is_user_interested = get_user_interest(context.exam.name)
+
+	context.metatags = {
+		"title": exam.title,
+		"image": exam.image,
+		"description": exam.short_introduction,
+		"keywords": exam.title,
+		"og:type": "website",
+	}
+
+
+def get_user_interest(exam):
+	return frappe.db.count(
+		"LMS Exam Interest", {"exam": exam, "user": frappe.session.user}
+	)
