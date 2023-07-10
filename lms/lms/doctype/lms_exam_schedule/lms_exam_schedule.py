528dd645ed14b434cd84abef2c552655cfdb08e4
Labeeb Mattra <mmlabeeb@gmail.com>
2023-07-10 15:22:11 +0530

    Add more fields to LMS Exam Schedule

--- lms/lms/doctype/lms_exam_schedule/lms_exam_schedule.py
+++ lms/lms/doctype/lms_exam_schedule/lms_exam_schedule.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2023, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import pytz
 from frappe.model.document import Document
 
 
 class LMSExamSchedule(Document):
-	pass
+
+	def validate(self):
+		self.validate_timezone()
+	
+	def validate_timezone(self):
+		if not self.timezone:
+			return
+
+		if self.timezone not in pytz.all_timezones:
+			frappe.throw(_("Invalid timezone."))
+
+
+	
+
