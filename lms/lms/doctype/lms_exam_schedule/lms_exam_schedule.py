9dcf0ec325b38c543eb74e8afb704a16e0f00d70
Labeeb Mattra <mmlabeeb@gmail.com>
2023-09-08 17:59:49 +0530

    Refactor LMS Exam Schedule

--- lms/lms/doctype/lms_exam_schedule/lms_exam_schedule.py
+++ lms/lms/doctype/lms_exam_schedule/lms_exam_schedule.py
@@ -37,26 +37,63 @@ class LMSExamSchedule(Document):
 
 
 	def before_submit(self):
-		self.total_marks, self.total_questions = update_questions_for_schedule(
-			self.name, self.questions
-		)
+		self.total_marks, self.total_questions = self.update_questions_for_schedule()
 	
 	def before_save(self):
 		if self.question_type != "Choices":
 			self.needs_evaluation = 1
 
 
-def validate_weightage_table(cat_weightage):
-	for cat in cat_weightage:
-		if not cat.mark_per_question or not cat.no_of_questions:
-			frappe.throw("No. of Qs & Marks per Qs columns for {} category should be more than 0.".format(
-				cat.question_category
-			))
-		
-		# check if selected no. of questions are available in question bank
-		get_random_questions(
-			cat.question_category, cat.mark_per_question, cat.no_of_questions
+	def validate_weightage_table(self):
+		for cat in self.questions:
+			if not cat.mark_per_question or not cat.no_of_questions:
+				frappe.throw("No. of Qs & Marks per Qs columns for {} category should be more than 0.".format(
+					cat.question_category
+				))
+
+			# check if selected no. of questions are available in question bank
+			get_random_questions(
+				cat.question_category, cat.mark_per_question, cat.no_of_questions,
+				self.question_type
+			)
+
+	def update_questions_for_schedule(self):
+		"""
+		Function to update assigned questions
+		> Validate if required no. of questions
+		> Delete existing questions
+		> Add questions
+
+		returns: total marks, no of marks
+		"""
+		self.validate_weightage_table()
+
+		existing_questions = frappe.get_all(
+			"Exam Schedule Question", {"exam_schedule": self.name}
 		)
+		for qs in existing_questions:
+			frappe.delete_doc("Exam Schedule Question", qs["name"])
+		
+		total_qs = 0
+		total_marks = 0
+		for cat in self.questions:
+			questions = get_random_questions(
+				cat.question_category, cat.mark_per_question,
+				cat.no_of_questions, self.question_type
+			)
+			for qs in questions:
+				doc = frappe.get_doc({
+						"doctype": "Exam Schedule Question",
+						"exam_schedule": self.name,
+						"question": qs["name"],
+						"mark": cat.mark_per_question,
+						"category": cat.question_category
+				})
+				doc.insert()
+				total_marks += cat.mark_per_question
+				total_qs += 1
+		
+		return	total_marks, total_qs
 
 
 def get_random_questions(category, mark_per_qs, no_of_qs, question_type):
@@ -77,49 +114,3 @@ def get_random_questions(category, mark_per_qs, no_of_qs, question_type):
 				"Insufficient no. of {} mark questions in {} category. Available: {}".format(
 				mark_per_qs, category, len(cat_qs)
 			))
-
-
-def update_questions_for_schedule(exam_schedule, cat_weightage):
-	"""
-	Function to update assigned questions
-	> Validate if required no. of questions
-	> Delete existing questions
-	> Add questions
-
-	returns: total marks, no of marks
-	"""
-	validate_weightage_table(cat_weightage)
-	existing_questions = frappe.get_all(
-		"Exam Schedule Question", {"exam_schedule": exam_schedule}
-	)
-	for qs in existing_questions:
-		frappe.delete_doc("Exam Schedule Question", qs["name"])
-	
-	question_type = frappe.db.get_value(
-		"LMS Exam Schedule", exam_schedule, "question_type"
-	)
-	
-	total_qs = 0
-	total_marks = 0
-	for cat in cat_weightage:
-		questions = get_random_questions(
-			cat.question_category, cat.mark_per_question,
-			cat.no_of_questions, question_type
-		)
-		for qs in questions:
-			doc = frappe.get_doc({
-					"doctype": "Exam Schedule Question",
-					"exam_schedule": exam_schedule,
-					"question": qs["name"],
-					"mark": cat.mark_per_question,
-					"category": cat.question_category
-			})
-			doc.insert()
-			total_marks += cat.mark_per_question
-			total_qs += 1
-	
-	return	total_marks, total_qs
-
-
-
-
