b5395869ab3689d27fb09b9e270501782179771b
Labeeb Mattra <mmlabeeb@gmail.com>
2023-09-21 11:42:03 +0530

    Return tab change values with live exam data

--- lms/overrides/user.py
+++ lms/overrides/user.py
@@ -271,7 +271,8 @@ def get_live_exam(member=None):
 			"submission_status": submission["status"],
 			"duration": schedule.duration,
 			"enable_calculator": exam.enable_calculator,
-			"allowed_tab_change": exam.allowed_tab_change,
+			"restrict_tab_changes": exam.restrict_tab_changes,
+			"allowed_tab_change": exam.allowed_tab_changes,
 			"live_status": ""
 		}
 
