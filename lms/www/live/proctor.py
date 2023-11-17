0809a094268f266f3e7a11183fe9bace52ec9275
Labeeb Mattra <mmlabeeb@gmail.com>
2023-11-17 08:19:52 +0530

    Get list of submissions for proctor

---
+++ lms/www/live/proctor.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import frappe
+from frappe import _
+from lms.lms.doctype.lms_exam_submission.lms_exam_submission import proctor_list
+
+def get_context(context):
+	"""
+	Get the active exams the logged-in user proctoring
+	"""
+	context.no_cache = 1
+
+	if frappe.session.user == "Guest":
+		raise frappe.PermissionError(_("Please login to access this page."))
+
+	context.page_context = {}
+	context.submissions = proctor_list()["submissions"]
+
